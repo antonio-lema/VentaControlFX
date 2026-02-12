0aabdba222282e052e869a1e14f5d507edd0b3e3
antonio-lema <antonio_lema@outlook.es>
2026-02-12 14:02:23 +0100

    feat: add equals and hashCode to Category model
    Implementa métodos equals y hashCode basados en el ID de la categoría. Esto permite comparar categorías correctamente cuando se cargan desde diferentes fuentes o listas, necesario para el sistema de selección de categorías activas en la UI.

--- src/main/java/com/mycompany/ventacontrolfx/model/Category.java
+++ src/main/java/com/mycompany/ventacontrolfx/model/Category.java
@@ -3,13 +3,37 @@ package com.mycompany.ventacontrolfx.model;
 public class Category {
     private int id;
     private String name;
+    private boolean visible;
+    private boolean favorite;
 
     public Category() {
+        this.visible = true;
+        this.favorite = false;
+    }
+
+    public Category(int id, String name, boolean visible, boolean favorite) {
+        this.id = id;
+        this.name = name;
+        this.visible = visible;
+        this.favorite = favorite;
+    }
+
+    public Category(int id, String name, boolean visible) {
+        this(id, name, visible, false);
     }
 
     public Category(int id, String name) {
-        this.id = id;
+        this(id, name, true, false);
+    }
+
+    public Category(String name, boolean visible, boolean favorite) {
         this.name = name;
+        this.visible = visible;
+        this.favorite = favorite;
+    }
+
+    public Category(String name) {
+        this(name, true, false);
     }
 
     public int getId() {
@@ -26,6 +50,37 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
     }
 
     @Override
